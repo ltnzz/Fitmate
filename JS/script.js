487d208cca1ff8df697fe1d73fe8f7df054b43c5
Arzza Munabim <aprilabim2804@gmail.com>
2025-07-04 14:07:21 +0700

    homepage and footer

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentYearSpan = document.getElementById('current-year');
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+});
